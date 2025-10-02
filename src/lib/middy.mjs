b499312c2b4a351a1b3f313927663ba4097df54e
LindbergSala <3ggjs24lindpa@folkuniversitetet.nu>
2025-10-02 15:10:47 +0200

    middy_Update

--- src/lib/middy.mjs
+++ src/lib/middy.mjs
@@ -1,10 +1,13 @@
-import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
-import httpErrorHandler from "@middy/http-error-handler";
-import httpCors from "@middy/http-cors";
+import middy from '@middy/core'
+import httpJsonBodyParser from '@middy/http-json-body-parser'
+import httpErrorHandler from '@middy/http-error-handler'
+import httpCors from '@middy/http-cors'
 
-export const withHttp = (handler) =>
-  middy(handler)
-    .use(httpJsonBodyParser())
+const wrap = (fn) =>
+  middy(fn)
+    .use(httpJsonBodyParser({ disableContentTypeError: true }))
     .use(httpErrorHandler())
-    .use(httpCors());
+    .use(httpCors())
+
+export const withHttp = wrap
+export const safeHandler = wrap
